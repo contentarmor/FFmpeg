803c04d77ff9c466c91419079852bac3e61c4724
ContentArmor <stf-support@synamedia.com>
2026-05-13 15:17:53 +0200

    Set PKG_VERS to 1.5.1

--- contentarmor/build.sh
+++ contentarmor/build.sh
@@ -9,7 +9,7 @@ FFMPEG_BUILD_DIR="${ROOT_DIR}/ffmpeg_out"
 
 DEBFULLNAME="Synamedia ContentArmor Watermarking"
 DEBEMAIL="stf-support@synamedia.com"
-PKG_VERS="1.5.0"
+PKG_VERS="1.5.1"
 CONTENT_ARMOR_HOME="/cafvm"
 FFMPEG_BUILD_LIB="./ffmpeg_out/lib"
 OUT_DEBS_DIR="./debs"
